b51e9a78b5a66ca1cf2ab46d65bb191f078c2f16
FrantzXY <francis.xiang@mail.utoronto.ca>
2024-12-30 13:15:29 -0500

    Import Matplotlib

--- data-cleaning.py
+++ data-cleaning.py
@@ -2,4 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib as mp
 
-source_df = pd.read_csv('data/source.csv')
+train = pd.read_csv('train.csv')
+print(train)
+
+categorical_columns = train.select_dtypes(include=['object', 'category']).columns.tolist()
+numerical_columns = train.select_dtypes(include=['number']).columns.tolist()
+
+print("Categorical Columns:", categorical_columns)
+print("Numerical Columns:", numerical_columns)
+
+# Linear interpolation for numerical columns
+train[numerical_columns] = train[numerical_columns].interpolate(method='linear', limit_direction='both')
+
+# Fill categorical columns with most frequent value (mode) in each column
+for col in categorical_columns:
+    train[col] = train[col].fillna(train[col].mode()[0])
+
+print(train.isnull().sum())
+
+print(train)
