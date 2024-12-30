dc489d54cf29760d5f53ac3349262903f6eaac99
Rundong Hua <157993340+stevenhua0320@users.noreply.github.com>
2024-12-30 19:56:44 +0000

    add comment for later inspection on imputation method

--- data-cleaning.py
+++ data-cleaning.py
@@ -14,7 +14,8 @@ print("Numerical Columns:", numerical_columns)
 # Linear interpolation for numerical columns
 train[numerical_columns] = train[numerical_columns].interpolate(method='linear', limit_direction='both')
 
-# Fill categorical columns with most frequent value (mode) in each column
+# Fill categorical columns with most frequent value (mode) in each column（we assume that the missing columns should be imputed with mode value, we would check back later
+# whether this is appropriate）
 for col in categorical_columns:
     train[col] = train[col].fillna(train[col].mode()[0])
 
