e47c9e6e45e753010f8fd3c86bd3367e9f053367
stevenhua0320 <r.hua@mail.utoronto.ca>
2024-12-31 18:34:43 -0500

    add comment for part2 & add potential training interpolation

--- data-cleaning.py
+++ data-cleaning.py
@@ -17,6 +17,8 @@ print("Numerical Columns:", numerical_columns)
 
 # Linear interpolation for numerical columns in data
 train[numerical_columns] = train[numerical_columns].interpolate(method='linear', limit_direction='both')
+# train[numerical_columns] = train[numerical_columns].interpolate(method="polynomial", order=2)
+
 
 # Fill categorical columns with most frequent value (mode) in each column
 # (we assume that the missing columns should be imputed with mode value, we would check back later
@@ -25,4 +27,9 @@ for col in categorical_columns:
     train[col] = train[col].fillna(train[col].mode()[0])
 
 print(train.isnull().sum())
+
+# Part 2 Since besides efs and efs_time, we have remaining 57 variables, which is feasible to analyze the trend
+# and perform the classification work. As in the project efs=0 means we do not know whether the observation
+# confront the event, this step aims to classify the efs=0 population with "highly probable" efs=1
+# population and the others remain with efs=0.
 
